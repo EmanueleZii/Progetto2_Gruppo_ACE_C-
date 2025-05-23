97b029a0e9189cfe004f811068947d0bc14dff47
Andrearttr <andrearottura03@gmail.com>
2025-05-23 17:44:16 +0200

    Update Program.cs

--- Progetto_Andrea/Program.cs
+++ Progetto_Andrea/Program.cs
@@ -2,9 +2,11 @@
 
 public class Dipendente
 {
+    //attributi privati
     private string nome;
     private int eta;
 
+    //proprietà pubbliche
     public string Nome
     {
         get { return nome; }
@@ -26,27 +28,32 @@ public class Dipendente
         }
     }
 
+    //costruttore
     public Dipendente(string _nome, int _eta)
-        {
-            Nome = _nome;
-            Eta = _eta;
-        }
+    {
+        Nome = _nome;
+        Eta = _eta;
+    }
     public virtual void EseguiCompito()
     {
         Console.WriteLine("Compito generico del dipendente.");
     }
 }
 
+//classe derivata 
 public class Meccanico : Dipendente
 {
+    //attributi privati
     private string specializzazione;
 
+    //proprietà pubbliche
     public string Specializzazione
     {
         get { return specializzazione; }
         set { specializzazione = value; }
     }
 
+    //costruttore
     public Meccanico(string _nome, int _eta, string _specializzazione) : base(_nome, _eta)
     {
         Specializzazione = _specializzazione;
@@ -57,3 +64,16 @@ public class Meccanico : Dipendente
     }
 }
 
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Dipendente> dipendenti = new List<Dipendente>();
+        bool exit = false;
+
+        while (!exit)
+        {
+            
+        }
+    }
+}
